0a6556dd74de8862801d76f0be4689a7dea31f97
Luiz Lelis <luizhlelis@gmail.com>
2022-02-10 23:23:57 -0300

    chore: add instrumentation to api

--- src/SignozPlayground.Api/Startup.cs
+++ src/SignozPlayground.Api/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace SignozPlayground.Api
 {
@@ -26,6 +29,20 @@ namespace SignozPlayground.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Adding the OtlpExporter creates a GrpcChannel.
+            // This switch must be set before creating a GrpcChannel/HttpClient when calling an insecure gRPC service.
+            // See: https://docs.microsoft.com/aspnet/core/grpc/troubleshoot#call-insecure-grpc-services-with-net-core-client
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            services.AddOpenTelemetryTracing(builder => builder
+                .AddAspNetCoreInstrumentation()
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(Configuration["Otlp:ServiceName"]))
+                .AddOtlpExporter(otlpOptions =>
+                {
+                    otlpOptions.Endpoint = new Uri(Configuration.GetValue<string>("Otlp:Endpoint"));
+                })
+            );
+
+            services.Configure<AspNetCoreInstrumentationOptions>(Configuration.GetSection("AspNetCoreInstrumentation"));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
